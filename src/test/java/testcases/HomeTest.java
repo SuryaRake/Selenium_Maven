acd1260b5266f6255245a06b670a9fc14d5b97fd
SuryaRake <su257417@wipro.com>
2022-09-23 23:23:00 +0530

    Adding updates

--- src/test/java/testcases/HomeTest.java
+++ src/test/java/testcases/HomeTest.java
@@ -2,10 +2,13 @@ package testcases;
 
 import org.testng.annotations.Test;
 
+import com.relevantcodes.extentreports.LogStatus;
+
 public class HomeTest extends BaseClass{
 	
 	@Test
 	public void Test1() {
+		test.log(LogStatus.INFO, "HomeTest Started");
 		
 		System.out.println("Inside home testcase");
 		
--- src/test/java/testcases/Readme.java
+++ src/test/java/testcases/Readme.java
@@ -13,6 +13,22 @@ public class Readme {
 	 * 
 	 * now download compatible chromedriver as per your chrome version.
 	 * paste the downloaded chromedriver inside the created maven project.
+	 * 
+	 * 
+	 * 
+	 * 
+	 * Use case 1:
+	 * Someone else want to execute your test cases, how do they make use of our script
+	 * How can it deal without  a user precence
+	 * 
+	 * Use case 2: Execute ur test suite automatically when a change is appear in the application
+	 * 
+	 * 
+	 * 
+	 *User 3: Execute the code in other system, so that my system wont be occupied, I can do scripting for other modules mean time
+	 *
+	 *For all these three use case, just use Jenkins Agent
+	 *
 	 */
 
 }
